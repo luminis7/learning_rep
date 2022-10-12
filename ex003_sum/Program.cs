fd1a9d302ce7a18f7bb7228b5d7f7ef2517b87f2
luminis7 <luminis@inbox.ru>
2022-10-12 22:21:12 +0300

    add randome

--- ex003_sum/Program.cs
+++ ex003_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int num1 = 1;
-int num2 = 2;
+﻿int num1 = new Random().Next(1,5);
+Console.WriteLine(num1);
+int num2 = new Random().Next(1,5);
+Console.WriteLine(num2);
 int sum = num1 + num2;
 Console.WriteLine(sum);
