664c254b7758665274151ff11e9532420cd87aa3
luminis7 <luminis@inbox.ru>
2022-10-12 22:41:10 +0300

    add if else

---
+++ ex005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "света")
+{
+    Console.WriteLine("Ура, это Света!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
